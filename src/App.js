a3f017c8af1b4ff65478e550451604661765e33b
Dan Burnside <dburnside22@gmail.com>
2019-07-01 17:32:48 -0500

    hook up redux and routing

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import './App.scss';
 import logo from './assets/logo.png';
+import Recipes from './components/Recipes/Recipes';
 
 function App() {
   return (
     <div className="App">
       <div className="header">
-        <img src={logo} />
+        <img src={logo} alt='Background of food'/>
 
       </div>
-      <p>Recipes of the day</p>
-      
+      <Recipes></Recipes>
+
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,8 +3,41 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { createStore } from "redux";
+import { Provider } from 'react-redux';
+import { Route, BrowserRouter as Router } from 'react-router-dom';
+import Meal from './components/Meal/Meal';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const initialState = {
+    meals: []
+}
+
+function reducer(state = initialState, action) {
+    switch (action.type){
+        case "CHANGERANDOMMEALS":
+            console.log('meals', action.payload.meals);
+            return {
+                meals: action.payload.meals
+            };
+        default: 
+            return state;
+        }
+}
+
+const store = createStore(reducer);
+
+const routing = (
+    <Router>
+        <Provider store={store}>
+            <div>
+                <Route exact path="/" component={App} />
+                <Route path="/:id" component={Meal} />
+            </div>
+      </Provider>
+    </Router>
+  )
+
+ReactDOM.render(routing, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
